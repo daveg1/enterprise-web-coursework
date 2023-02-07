ac8be3e7742a38125d86cb4bd5eb32269923bf5d
David Graham <d.graham8@rgu.ac.uk>
2023-02-07 20:42:59 +0000

    add signup form; add User type

--- angular/src/app/pages/signup/signup.component.ts
+++ angular/src/app/pages/signup/signup.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { NonNullableFormBuilder } from '@angular/forms';
+import type { User } from 'src/app/types/User';
 
 @Component({
-  selector: 'app-signup',
-  templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+	selector: 'app-signup',
+	templateUrl: './signup.component.html',
+	styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent {
+	signupForm;
 
+	constructor(readonly formBuilder: NonNullableFormBuilder) {
+		this.signupForm = this.formBuilder.group<User>({
+			firstname: '',
+			lastname: '',
+			username: '',
+			password: '',
+		});
+	}
+
+	submitForm() {
+		if (this.signupForm.valid) {
+			console.log(this.signupForm.value);
+		} else {
+			console.log('Form is not valid', this.signupForm.value);
+		}
+	}
 }
---
+++ angular/src/app/types/User.d.ts
@@ -0,0 +1,7 @@
+export type User = {
+	firstname: string;
+	lastname: string;
+	username: string;
+	password: string;
+	quotes?: any[];
+};
