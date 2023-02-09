b3f093b837d28be524914e0ff5481adc9ae90c01
David Graham <d.graham8@rgu.ac.uk>
2023-02-09 16:40:22 +0000

    api: remove signup and login routes; move /signup route to /account/create

--- api/src/routes/account.route.ts
+++ api/src/routes/account.route.ts
@@ -1,5 +1,28 @@
-import { Router } from 'express'
+import { Router, urlencoded } from 'express'
+import { User } from '../models/user.model'
+import { userSchema } from '../schemas/user.schema'
 
 const accountRoutes = Router()
+
+// TODO: encrypt passwords before storing on database
+accountRoutes.post('/create', urlencoded({ extended: true }), async (req, res) => {
+	try {
+		const parsed = await userSchema.parseAsync(req.body)
+
+		const regex = new RegExp(parsed.username, 'i')
+		const user = await User.findOne({ username: { $regex: regex } }, { password: 0 })
+
+		if (user) {
+			return res.status(400).json({ reason: 'That username already exists. Please choose another' })
+		}
+
+		const newUser = new User(parsed)
+		await newUser.save()
+
+		res.status(200).json({ firstname: parsed.firstname })
+	} catch (error) {
+		res.status(500).json({ error })
+	}
+})
 
 export { accountRoutes }
--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -1,14 +1,10 @@
 import { Router } from 'express'
 import { accountRoutes } from './account.route'
 import { budgetRoutes } from './budget.route'
-import { loginRoutes } from './login.route'
-import { signupRoutes } from './signup.route'
 
 const routes = Router()
 
 routes.use('/account', accountRoutes)
 routes.use('/budget', budgetRoutes)
-routes.use('/login', loginRoutes)
-routes.use('/signup', signupRoutes)
 
 export { routes }
