2530e296b1b0e19cfcc84deafdf2d8a67f39140a
David Graham <d.graham8@rgu.ac.uk>
2023-02-11 17:20:46 +0000

    add types for auth

---
+++ angular/src/app/types/auth.d.ts
@@ -0,0 +1,11 @@
+export type AuthSignUp = {
+	firstname: string;
+	lastname: string;
+	username: string;
+	password: string;
+};
+
+export type AuthLogin = {
+	username: string;
+	password: string;
+};
