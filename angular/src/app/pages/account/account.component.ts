88597d4276caea09deb3342485fb73ceb51f115f
David Graham <d.graham8@rgu.ac.uk>
2023-02-11 18:22:46 +0000

    add redirect from /account if not logged in

--- angular/src/app/pages/account/account.component.ts
+++ angular/src/app/pages/account/account.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
-  selector: 'app-account',
-  templateUrl: './account.component.html',
-  styleUrls: ['./account.component.css']
+	selector: 'app-account',
+	templateUrl: './account.component.html',
+	styleUrls: ['./account.component.css'],
 })
 export class AccountComponent {
-
+	constructor(
+		private readonly authService: AuthService,
+		readonly router: Router,
+		readonly route: ActivatedRoute
+	) {
+		this.authService.isLoggedIn$.subscribe((isLoggedIn) => {
+			console.log(isLoggedIn);
+			if (!isLoggedIn) {
+				this.router.navigate(['/']);
+			}
+		});
+	}
 }
