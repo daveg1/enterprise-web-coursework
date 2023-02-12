613dd19ef65757872505b6276e20822b49a0ac64
David Graham <d.graham8@rgu.ac.uk>
2023-02-12 23:18:33 +0000

    api: fix user authentication in login

--- api/src/routes/auth.route.ts
+++ api/src/routes/auth.route.ts
@@ -2,7 +2,7 @@ import { Router, urlencoded } from 'express'
 import { User } from '../models/user.model'
 import { loginSchema, signUpSchema } from '../schemas/auth.schema'
 import jwt from 'jsonwebtoken'
-import { encrypt } from '../modules/encrypt'
+import { encrypt, verify } from '../modules/encrypt'
 
 const authRoutes = Router()
 
@@ -34,22 +34,31 @@ authRoutes.post('/signup', async (req, res) => {
 authRoutes.post('/login', async (req, res) => {
 	try {
 		const parsed = await loginSchema.parseAsync(req.body)
-
 		const regex = new RegExp(parsed.username, 'i')
-		const user = await User.findOne(
-			{ username: { $regex: regex }, password: parsed.password },
-			{ password: 0 },
-		)
+
+		// Retreive user from DB
+		const user = await User.findOne({ username: { $regex: regex } }, { username: 1, password: 1 })
 
 		if (!user) {
 			return res.status(400).json({
-				reason: "Sorry, that username and password combination didn't work. Please try again.",
+				reason: "Sorry, that user doesn't exist. Please check your spelling.",
 			})
 		}
 
-		// JWT
-		const token = jwt.sign(user.toJSON(), process.env.ACCESS_TOKEN)
-		res.status(200).json({ token })
+		// Compare password with hashed password
+		const passwordIsValid = await verify(parsed.password, user.password)
+
+		if (!passwordIsValid) {
+			return res.status(400).json({
+				reason: "Sorry, that password isn't correct. Please try again.",
+			})
+		}
+
+		// Sign JWT using username and password
+		const { username, password } = user.toJSON()
+		const token = jwt.sign(username + password, process.env.ACCESS_TOKEN)
+
+		res.status(200).json({ username, token, quotes: user.quotes ?? [] })
 	} catch (error) {
 		console.log(error)
 		return res.status(500).json({ error })
