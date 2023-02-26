dcd7295af5e271d51605c8502d285a4d392dcd8c
David Graham <d.graham8@rgu.ac.uk>
2023-02-26 18:59:21 +0000

    add node-cleanup for gracefully exiting process

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,11 +1,14 @@
 import 'dotenv/config'
 import cors from 'cors'
 import express from 'express'
-import { routes } from './routes'
+import http from 'http'
+import nodeCleanup from 'node-cleanup'
 import { connectDatabase } from './modules/connectDatabase'
 import { timestamp } from './modules/timestamp'
+import { routes } from './routes'
 
 const app = express()
+const server = http.createServer(app)
 
 // Set app variables
 app.set('port', process.env.PORT || 8080)
@@ -23,10 +26,15 @@ connectDatabase()
 	.then(() => {
 		console.log(timestamp(), 'Mongo connection made')
 
-		app.listen(app.get('port'), () => {
+		server.listen(app.get('port'), () => {
 			console.log(timestamp(), `Server listening at http://localhost:${app.get('port')}`)
 		})
 	})
 	.catch((error) => {
 		console.error(timestamp(), 'Mongo error while making connection', error)
 	})
+
+nodeCleanup(() => {
+	console.log(timestamp(), `Server closing...`)
+	server.close()
+})
