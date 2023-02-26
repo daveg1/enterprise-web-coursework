7e310047d42829d13579073f8c009c57c07695a8
David Graham <d.graham8@rgu.ac.uk>
2023-02-26 20:04:22 +0000

    api: add user id to saved quotes

--- api/src/routes/quote.route.ts
+++ api/src/routes/quote.route.ts
@@ -1,5 +1,7 @@
 import { Router } from 'express'
+import jwt from 'jsonwebtoken'
 import { Quote } from '../models/quote.model'
+import { User } from '../models/user.model'
 import { calculateQuote } from '../modules/calculateQuote'
 import { budgetSchema } from '../schemas/budget.schema'
 import { quoteSchema } from '../schemas/quotes.schema'
@@ -21,11 +23,25 @@ quoteRoutes.post('/save', async (req, res) => {
 	try {
 		const parsed = await quoteSchema.parseAsync(req.body)
 
-		const quote = new Quote(parsed)
+		// Verify user token
+		const userId = jwt.decode(parsed.token)
+		const user = await User.findById(userId)
+
+		if (!user) {
+			res.status(401).json({ message: 'quote/save POST No user by that id' })
+		}
+
+		const quote = new Quote({
+			budget: parsed.budget,
+			projectName: parsed.projectName,
+			user: userId,
+		})
+
 		await quote.save()
 
 		res.status(200).json({ message: 'success' })
 	} catch (error) {
+		console.log(error)
 		res.status(500).json({ error })
 	}
 })
