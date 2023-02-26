c5651b91a776d3cf1d64eae50f3d52f956efe8ca
David Graham <d.graham8@rgu.ac.uk>
2023-02-26 17:23:18 +0000

    angular: add budget service

---
+++ angular/src/app/services/budget.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Budget } from '../types/Budget';
+
+/**
+ * This service keeps track of the current values in the quote calculator form.
+ *
+ * This is not to be confused with the quote service, which deals with the actual quote values.
+ */
+@Injectable({
+	providedIn: 'root',
+})
+export class BudgetService {
+	currentBudget?: Budget;
+}
