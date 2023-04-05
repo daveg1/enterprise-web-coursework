d714772ea6139558a8ee6691122df2f48280027b
David Graham <d.graham8@rgu.ac.uk>
2023-04-05 19:00:18 +0100

    api: add paygradeRoutes to main router

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 import { authRoutes } from './auth.route'
+import { paygradeRoutes } from './paygrade.route'
 import { quoteRoutes } from './quote.route'
 
 const routes = Router()
 
 routes.use('/auth', authRoutes)
 routes.use('/quote', quoteRoutes)
+routes.use('/paygrade', paygradeRoutes)
 
 export { routes }
