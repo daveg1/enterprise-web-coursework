169b824ed2f2e118c023e34049e0ce7f74ec6412
David Graham <d.graham8@rgu.ac.uk>
2023-04-06 16:54:05 +0100

    angular: reverse previous refactor

--- angular/src/app/components/calculator-form/calculator-form.component.ts
+++ angular/src/app/components/calculator-form/calculator-form.component.ts
@@ -21,7 +21,7 @@ import { QuoteWholeResponse } from 'src/app/types/quote';
 export class CalculatorFormComponent {
 	@Output() quoteCalculated = new EventEmitter<number>();
 
-	readonly form;
+	readonly budgetForm;
 	// the following are used in the template
 	readonly timeUnits = timeUnits;
 	readonly payGrades = payGrades;
@@ -33,15 +33,15 @@ export class CalculatorFormComponent {
 		readonly fb: NonNullableFormBuilder,
 		readonly dialog: DialogService
 	) {
-		this.form = this.fb.group({
+		this.budgetForm = this.fb.group({
 			workers: this.fb.array([this.fb.group(workerForm)]),
 			oneOffCosts: this.fb.array([this.fb.group(oneOffCostForm)]),
 			ongoingCosts: this.fb.array([this.fb.group(ongoingCostForm)]),
 		});
 
 		// Remove one-off and ongoing cost rows (by default only have a worker row)
-		this.form.controls['oneOffCosts'].removeAt(0);
-		this.form.controls['ongoingCosts'].removeAt(0);
+		this.budgetForm.controls['oneOffCosts'].removeAt(0);
+		this.budgetForm.controls['ongoingCosts'].removeAt(0);
 	}
 
 	/**
@@ -49,7 +49,7 @@ export class CalculatorFormComponent {
 	 * Generates rows based on the fields in the provided quote object.
 	 */
 	setQuote(quote: QuoteWholeResponse) {
-		this.form.controls['workers'].removeAt(0);
+		this.budgetForm.controls['workers'].removeAt(0);
 
 		quote.budget.workers.forEach((worker) => {
 			const row = this.fb.group(workerForm);
@@ -57,7 +57,7 @@ export class CalculatorFormComponent {
 			row.controls['timeUnit'].setValue(worker.timeUnit);
 			row.controls['timeWorked'].setValue(worker.timeWorked);
 
-			this.form.controls['workers'].push(row);
+			this.budgetForm.controls['workers'].push(row);
 		});
 
 		quote.budget.oneOffCosts.forEach((oneOffCost) => {
@@ -65,7 +65,7 @@ export class CalculatorFormComponent {
 			row.controls['cost'].setValue(oneOffCost.cost);
 			row.controls['itemName'].setValue(oneOffCost.itemName);
 
-			this.form.controls['oneOffCosts'].push(row);
+			this.budgetForm.controls['oneOffCosts'].push(row);
 		});
 
 		quote.budget.ongoingCosts.forEach((ongoingCost) => {
@@ -73,52 +73,52 @@ export class CalculatorFormComponent {
 			row.controls['cost'].setValue(ongoingCost.cost);
 			row.controls['itemName'].setValue(ongoingCost.itemName);
 
-			this.form.controls['ongoingCosts'].push(row);
+			this.budgetForm.controls['ongoingCosts'].push(row);
 		});
 	}
 
 	submitForm() {
-		if (this.form.valid) {
+		if (this.budgetForm.valid) {
 			this.quoteService
-				.calculateQuote(this.form.value as Budget)
+				.calculateQuote(this.budgetForm.value as Budget)
 				.subscribe((res) => {
 					this.quoteCalculated.emit(res.quote);
 				});
 		} else {
-			this.form.markAllAsTouched();
+			this.budgetForm.markAllAsTouched();
 		}
 	}
 
 	saveQuote() {
-		this.budgetService.currentBudget = this.form.value as Budget;
+		this.budgetService.currentBudget = this.budgetForm.value as Budget;
 		this.dialog.show();
 	}
 
 	addWorker() {
 		const worker = this.fb.group(workerForm);
-		this.form.controls['workers'].push(worker);
+		this.budgetForm.controls['workers'].push(worker);
 	}
 
 	removeWorker(index: number) {
-		this.form.controls['workers'].removeAt(index);
+		this.budgetForm.controls['workers'].removeAt(index);
 	}
 
 	addOneOffCost() {
 		const oneOffCost = this.fb.group(oneOffCostForm);
-		this.form.controls['oneOffCosts'].push(oneOffCost);
+		this.budgetForm.controls['oneOffCosts'].push(oneOffCost);
 	}
 
 	removeOneOffCost(index: number) {
-		this.form.controls['oneOffCosts'].removeAt(index);
+		this.budgetForm.controls['oneOffCosts'].removeAt(index);
 	}
 
 	addOngoingCost() {
 		const ongoingCost = this.fb.group(ongoingCostForm);
 
-		this.form.controls['ongoingCosts'].push(ongoingCost);
+		this.budgetForm.controls['ongoingCosts'].push(ongoingCost);
 	}
 
 	removeOngoingCost(index: number) {
-		this.form.controls['ongoingCosts'].removeAt(index);
+		this.budgetForm.controls['ongoingCosts'].removeAt(index);
 	}
 }
