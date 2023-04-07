aaa6ca2b5c4d41febe138b55c8f6f68092b755af
David Graham <d.graham8@rgu.ac.uk>
2023-04-07 18:46:09 +0100

    angular: replace primary and secondary buttons with button directive

--- angular/src/app/components/buttons/primary-button/primary-button.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, Input } from '@angular/core';
-
-@Component({
-	selector: 'app-primary-button',
-	templateUrl: './primary-button.component.html',
-})
-export class PrimaryButtonComponent {
-	@Input() text = '';
-}
